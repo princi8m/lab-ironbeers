5e47db3b192aeb85375a644941c5239b4543a21b
MP <princi8@gmail.comm>
2021-02-05 10:58:37 +0100

    Troubled updateto be fixed

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 const express = require('express');
-
-const hbs = require('hbs');
-const path = require('path');
+const hbs     = require('hbs');
+const app     = express();
+const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
-// ...
-
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
+// Index Page
+app.get('/', (req, res, next) => {
+  const data = {
+    title: "Home"
+  }
+res.render('index', data);
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// Beers Button
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beers => {const data = {
+        title: "Beers",
+        beers: beers
+      }
+      res.render('beers', data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+});
+
+
+
+
+app.listen(3001, () => console.log("Server running at localhost:3001"));
